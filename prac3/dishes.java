127b289c3caa2bfbb93dd39b1ec87437c457652f
Alex Wortega <alexdragannikolich@gmail.com>
2020-09-23 20:06:51 +0300

    Update dishes.java

--- prac3/dishes.java
+++ prac3/dishes.java
@@ -1,9 +1,31 @@
-public class Dish {
-    public int S;
-    public int cout;
-    public bool
-    public static void main(String[] args) {
+import java.util.Scanner;
+public abstract class Dish {
+    private int S;
+    private int cout;
+    private float mass;
+    public void fill(){
+    System.out.println("filled");
 
+    }
+    public abstract void pal();
+    public abstract void thro();
+    public void cut(){
+        System.out.println("cutted");
+    }
+    public void setS(int s)
+    {
+        S = s;
+    }
+    public void setMass(float m)
+    {
+        mass = m;
+    }
+    public void setCout(int cou){
+        cout = cou;
+    }
+
+    public String toString(){
+       return (" S = "+ S +" Mass = "+mass+" Cout = "+cout);
 
     }
 
