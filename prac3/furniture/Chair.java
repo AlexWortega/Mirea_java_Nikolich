f84ac84714e3365c930c2285237610459b9acfe0
Alex Wortega <alexdragannikolich@gmail.com>
2020-09-23 20:05:08 +0300

    Create Chair.java

---
+++ prac3/furniture/Chair.java
@@ -0,0 +1,25 @@
+public class Chair extends Furniture {
+    private boolean isBroken;
+
+    public void setBroken(boolean broken) {
+        isBroken = broken;
+    }
+
+    protected Chair(int price, String color, int length) {
+        super(price, color, length);
+    }
+
+    @Override
+    public void totrow() {
+        System.out.println("Зачем кидать стул.....");
+    }
+
+    @Override
+    public String toString() {
+        return "Chair{" +
+                "price=" + price +
+                ", color='" + color + '\'' +
+                ", length=" + length +
+                '}';
+    }
+}
